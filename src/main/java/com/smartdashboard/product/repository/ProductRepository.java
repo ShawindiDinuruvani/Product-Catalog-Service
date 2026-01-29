6b98fda228567feec42956b31db3b9803819fc93
Shawindi dinuruvani <shawindidinuruvani@gmail.com>
2026-01-29 21:09:14 +0530

    Implemented Product API

---
+++ src/main/java/com/smartdashboard/product/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.smartdashboard.product.repository;
+
+import com.smartdashboard.product.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
